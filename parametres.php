93b09da858e7f5a13a8a43ffca13a1aedd355f5f
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2020-10-14 16:55:07 +0200

    Création de la page paramtre et remplissage du php

---
+++ parametres.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define('DATABASE_URL', "localhost" );
+define('DATABASE_USER', "vedrenne");
+define('DATABASE_PWD',"P@ssw0rd");
+define('DATABASE_NAME',"clicom");
+define('CNSTRING',"mysql:host=" . DATABASE_URL . ";dbname=" . DATABASE_NAME . "charsert=UTF8");
