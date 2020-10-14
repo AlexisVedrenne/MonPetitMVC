6ef8d154958f5f8e0c72088818bc25a28c1860fa
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2020-10-14 17:17:02 +0200

    Création de la fonction chercherUn()

---
+++ src/Controller/GestionClientController.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace APP\Controller;
+
+/**
+ * Description of GestionClientController
+ *
+ * @author vedrenne.alexis
+ */
+
+use APP\Modele\gestionClientModel;
+use ReflectionClass;
+use \Exception;
+
+class GestionClientController {
+    
+    public function chercherUn($params){
+        $modele=new gestionClientModel();
+        $id=filter_var(intval($params["id"]),FILTER_VALIDATE_INT);
+        $unClient=$modele->find($id);
+        if($unClient){
+            $r=new ReflectionClass($this);
+            include_once PATH_VIEW . str_replace('Controller','View',$r->getShortName()) . "/unClient.php";
+        }
+        else{
+            throw new Exception("Client ". $id . "inconnu");
+        }
+    }
+}
---
+++ src/Controller/IdentificationController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IdentificationController
+ *
+ * @author vedrenne.alexis
+ */
+namespace APP\Controller;
+class IdentificationController {
+    //put your code here
+}
